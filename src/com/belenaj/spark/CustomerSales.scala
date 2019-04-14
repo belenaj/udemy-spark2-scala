e4b21c83f5235d66cd8a906e46d6a41ea3bf26d4
Jorge <jorgebelena@gmail.com>
2019-04-14 14:08:10 +0200

    adding customerSales exercise

--- src/com/belenaj/spark/CustomerSales.scala
+++ src/com/belenaj/spark/CustomerSales.scala
@@ -6,29 +6,38 @@ import org.apache.log4j._
 
 object CustomerSales {
 
+  def parseLine(line:String)= {
+    val fields = line.split(",")
+    val customerID = fields(0)
+    val productID = fields(1)
+    val amount = fields(2).toDouble
+    (customerID, productID, amount)
+  }
+
   /** Our main function where the action happens */
-  def main() {
+  def main(args: Array[String]) {
 
     // Set the log level to only print errors
     Logger.getLogger("org").setLevel(Level.ERROR)
 
     // Create a SparkContext using every core of the local machine
-    val sc = new SparkContext("local[*]", "MaxTemperatures")
+    val sc = new SparkContext("local[*]", "CustomerSales")
 
-    val lines = sc.textFile("../1800.csv")
+    val lines = sc.textFile("./src/resources/customer-orders.csv")
     val parsedLines = lines.map(parseLine)
-    val maxTemps = parsedLines.filter(x => x._2 == "TMAX")
+
+
+    val keyPair = parsedLines.map(x => (x._1, x._3))
+
+
+
+   /* val maxTemps = parsedLines.filter(x => x._2 == "TMAX")
     val stationTemps = maxTemps.map(x => (x._1, x._3.toFloat))
     val maxTempsByStation = stationTemps.reduceByKey( (x,y) => max(x,y))
     val results = maxTempsByStation.collect()
 
-    for (result <- results.sorted) {
-      val station = result._1
-      val temp = result._2
-      val formattedTemp = f"$temp%.2f F"
-      println(s"$station max temperature: $formattedTemp")
-    }
 
+*/
   }
 
 }
